469e3e2c51eafcfa27d38d50cd13d1c7e12f6d79
dongcool <95206510+dongcool@users.noreply.github.com>
2021-12-01 12:13:05 +0800

    more tests

--- tests/__tests__/staking-pool/staking-pool.ava.ts
+++ tests/__tests__/staking-pool/staking-pool.ava.ts
@@ -63,3 +63,154 @@ workspace.test('deposit and stake', async (test, {contract, alice}) => {
     deposit.sub(stakeAmount).toString()
   );
 });
+
+workspace.test('add reward', async (test, { contract, alice }) => {
+  // deposit
+  const deposit = NEAR.parse('10');
+
+  await alice.call(
+    contract,
+    'deposit',
+    {},
+    { attachedDeposit: deposit },
+  );
+
+  // stake
+  const stakeAmount = NEAR.parse('9');
+  await alice.call(
+    contract,
+    'stake',
+    { amount: stakeAmount.toString() }
+  );
+
+  // add reward
+  const reward = NEAR.parse('1');
+  await alice.call(
+    contract,
+    'add_reward',
+    { amount: reward.toString() }
+  );
+
+  test.is(
+    await contract.view('get_account_staked_balance', { account_id: alice }),
+    stakeAmount.add(reward).toString()
+  );
+  test.is(
+    await contract.view('get_account_unstaked_balance', { account_id: alice }),
+    deposit.sub(stakeAmount).toString()
+  );
+});
+
+workspace.test('unstake', async (test, { contract, alice }) => {
+  // deposit
+  const deposit = NEAR.parse('10');
+
+  await alice.call(
+    contract,
+    'deposit',
+    {},
+    { attachedDeposit: deposit },
+  );
+
+  // stake
+  const stakeAmount = NEAR.parse('9');
+  await alice.call(
+    contract,
+    'stake',
+    { amount: stakeAmount.toString() }
+  );
+
+  // add reward
+  const reward = NEAR.parse('1');
+  await alice.call(
+    contract,
+    'add_reward',
+    { amount: reward.toString() }
+  );
+
+  // unstake
+  const unstakeAmount = NEAR.parse('5');
+  await alice.call(
+    contract,
+    'unstake',
+    { amount: unstakeAmount.toString() }
+  );
+
+  test.is(
+    await contract.view('get_account_staked_balance', { account_id: alice }),
+    stakeAmount.add(reward).sub(unstakeAmount).toString()
+  );
+  test.is(
+    await contract.view('get_account_unstaked_balance', { account_id: alice }),
+    deposit.sub(stakeAmount).add(unstakeAmount).toString()
+  );
+});
+
+workspace.test('withdraw', async (test, { contract, alice }) => {
+  // deposit
+  const deposit = NEAR.parse('10');
+
+  await alice.call(
+    contract,
+    'deposit',
+    {},
+    { attachedDeposit: deposit },
+  );
+
+  // stake
+  const stakeAmount = NEAR.parse('9');
+  await alice.call(
+    contract,
+    'stake',
+    { amount: stakeAmount.toString() }
+  );
+
+  // add reward
+  const reward = NEAR.parse('1');
+  await alice.call(
+    contract,
+    'add_reward',
+    { amount: reward.toString() }
+  );
+
+  // first withdraw
+  const firstWithdrawAmount = NEAR.parse('0.5');
+  await alice.call(
+    contract,
+    'withdraw',
+    { amount: firstWithdrawAmount.toString() }
+  );
+
+  test.is(
+    await contract.view('get_account_staked_balance', { account_id: alice }),
+    stakeAmount.add(reward).toString()
+  );
+  test.is(
+    await contract.view('get_account_unstaked_balance', { account_id: alice }),
+    deposit.sub(stakeAmount).sub(firstWithdrawAmount).toString()
+  );
+
+  // unstake
+  const unstakeAmount = NEAR.parse('5');
+  await alice.call(
+    contract,
+    'unstake',
+    { amount: unstakeAmount.toString() }
+  ); 
+
+  // withdraw all
+  await alice.call(
+    contract,
+    'withdraw_all',
+    {}
+  );
+
+  test.is(
+    await contract.view('get_account_staked_balance', { account_id: alice }),
+    stakeAmount.add(reward).sub(unstakeAmount).toString()
+  );
+  test.is(
+    await contract.view('get_account_unstaked_balance', { account_id: alice }),
+    '0'
+  );
+});

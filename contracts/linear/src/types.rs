9a2c882498004e71214cffed4f9f5e8423e7a0c1
dongcool <95206510+dongcool@users.noreply.github.com>
2021-12-03 10:58:23 +0800

    type def (#10)

--- contracts/linear/src/types.rs
+++ contracts/linear/src/types.rs
@@ -3,18 +3,13 @@ use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{Balance, Gas};
 use uint::construct_uint;
 
-/// The amount of gas given to complete `vote` call.
-pub const VOTE_GAS: Gas = Gas(100_000_000_000_000);
+/// Type for stake share (LiNEAR) balance
+pub type ShareBalance = u128;
 
-/// The amount of gas given to complete internal `on_stake_action` call.
-pub const ON_STAKE_ACTION_GAS: Gas = Gas(20_000_000_000_000);
-
-/// The amount of yocto NEAR the contract dedicates to guarantee that the "share" price never
-/// decreases. It's used during rounding errors for share -> amount conversions.
-pub const STAKE_SHARE_PRICE_GUARANTEE_FUND: Balance = 1_000_000_000_000;
-
-/// There is no deposit balance attached.
+/// No deposit balance.
 pub const NO_DEPOSIT: Balance = 0;
+/// one NEAR
+pub const NEAR: Balance = 1_000_000_000_000_000_000_000_000;
 
 construct_uint! {
     /// 256-bit unsigned integer.

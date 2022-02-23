d54d23ff4c49915d6c52aaf53b796d502633beef
dongcool <95206510+dongcool@users.noreply.github.com>
2022-02-23 18:35:36 +0800

    get_beneficiaries make public

--- contracts/linear/src/owner.rs
+++ contracts/linear/src/owner.rs
@@ -2,15 +2,9 @@ use crate::*;
 use near_sdk::{
     near_bindgen,
 };
-use std::collections::HashMap;
 
 #[near_bindgen]
 impl LiquidStakingContract {
-    pub fn get_beneficiaries(& self) -> HashMap<AccountId, Fraction> {
-        self.assert_owner();
-        self.internal_get_beneficiaries()
-    }
-
     pub fn set_beneficiary(
         &mut self,
         account_id: AccountId,
--- contracts/linear/src/view.rs
+++ contracts/linear/src/view.rs
@@ -2,6 +2,7 @@ use crate::*;
 use near_sdk::{
     near_bindgen,
 };
+use std::collections::HashMap;
 
 /// public view functions
 #[near_bindgen]
@@ -12,5 +13,9 @@ impl LiquidStakingContract {
 
     pub fn get_total_staked_near_amount(& self) -> Balance {
         self.total_staked_near_amount
+    }
+
+    pub fn get_beneficiaries(& self) -> HashMap<AccountId, Fraction> {
+        self.internal_get_beneficiaries()
     }
 }

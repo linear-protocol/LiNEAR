f2f7d9cc4c9fdcfc838968028fba4735d449e3df
dongcool <95206510+dongcool@users.noreply.github.com>
2022-03-04 22:40:08 +0800

    panic

--- contracts/mock-staking-pool/src/lib.rs
+++ contracts/mock-staking-pool/src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{U128};
 use near_sdk::collections::{LookupMap};
-use near_sdk::{ env, near_bindgen, AccountId, PanicOnDefault, Promise };
+use near_sdk::{ env, near_bindgen, AccountId, PanicOnDefault, Promise, require };
 
 /// Staking pool interface
 trait StakingPool {
@@ -31,7 +31,9 @@ trait StakingPool {
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct MockStakingPool {
     deposits: LookupMap<AccountId, u128>,
-    staked: LookupMap<AccountId, u128>
+    staked: LookupMap<AccountId, u128>,
+    /// for testing purpose, simulates contract panic
+    panic: bool,
 }
 
 #[near_bindgen]
@@ -40,7 +42,8 @@ impl MockStakingPool {
     pub fn new() -> Self {
         Self {
             deposits: LookupMap::new(b"d"),
-            staked: LookupMap::new(b"s")
+            staked: LookupMap::new(b"s"),
+            panic: false,
         }
     }
 }
@@ -48,14 +51,17 @@ impl MockStakingPool {
 #[near_bindgen]
 impl StakingPool for MockStakingPool {
     fn get_account_staked_balance(&self, account_id: AccountId) -> U128 {
+        require!(!self.panic, "Test Panic!");
         U128::from(self.internal_get_staked(&account_id))
     }
 
     fn get_account_unstaked_balance(&self, account_id: AccountId) -> U128 {
+        require!(!self.panic, "Test Panic!");
         U128::from(self.internal_get_unstaked_deposit(&account_id))
     }
 
     fn get_account_total_balance(&self, account_id: AccountId) -> U128 {
+        require!(!self.panic, "Test Panic!");
         U128::from(
             self.internal_get_unstaked_deposit(&account_id) + self.internal_get_staked(&account_id)
         )
@@ -63,11 +69,13 @@ impl StakingPool for MockStakingPool {
 
     #[payable]
     fn deposit(&mut self) {
+        require!(!self.panic, "Test Panic!");
         self.internal_deposit()
     }
 
     #[payable]
     fn deposit_and_stake(&mut self) {
+        require!(!self.panic, "Test Panic!");
         let account_id = env::predecessor_account_id();
 
         self.internal_deposit();
@@ -77,25 +85,30 @@ impl StakingPool for MockStakingPool {
     }
 
     fn withdraw(&mut self, amount: U128) {
+        require!(!self.panic, "Test Panic!");
         let account_id = env::predecessor_account_id();
         self.internal_withdraw(&account_id, amount.0);
     }
 
     fn withdraw_all(&mut self) {
+        require!(!self.panic, "Test Panic!");
         let account_id = env::predecessor_account_id();
         let unstaked = self.internal_get_unstaked_deposit(&account_id);
         self.internal_withdraw(&account_id, unstaked);
     }
 
     fn stake(&mut self, amount: U128) {
+        require!(!self.panic, "Test Panic!");
         self.internal_stake(amount.0)
     }
 
     fn unstake(&mut self, amount: U128) {
+        require!(!self.panic, "Test Panic!");
         self.internal_unstake(amount.0);
     }
 
     fn unstake_all(&mut self) {
+        require!(!self.panic, "Test Panic!");
         let account_id = env::predecessor_account_id();
         let staked_amount = self.internal_get_staked(&account_id);
         self.internal_unstake(staked_amount);
@@ -113,6 +126,10 @@ impl MockStakingPool {
 
         let new_amount = staked_amount + amount.0;
         self.staked.insert(&account_id, &new_amount);
+    }
+
+    pub fn set_panic(&mut self, panic: bool) {
+        self.panic = panic;
     }
 }
 

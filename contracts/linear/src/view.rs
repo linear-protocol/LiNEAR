13ee341434698fc2c3ec2266285cccf70b2ce6eb
Linguists <95207870+linguists@users.noreply.github.com>
2022-03-08 01:46:51 +0800

    refactor: move staking pool view methods

--- contracts/linear/src/view.rs
+++ contracts/linear/src/view.rs
@@ -99,8 +99,85 @@ impl LiquidStakingContract {
           farms: self.get_active_farms(),
       }
     }
-  
-    // Staking Farm
+
+    // --- Staking Pool view methods ---
+
+    /// Returns the unstaked balance of the given account.
+    pub fn get_account_unstaked_balance(&self, account_id: AccountId) -> U128 {
+        self.get_account(account_id).unstaked_balance
+    }
+
+    /// Returns the staked balance of the given account.
+    /// NOTE: This is computed from the amount of "stake" shares the given account has and the
+    /// current amount of total staked balance and total stake shares on the account.
+    pub fn get_account_staked_balance(&self, account_id: AccountId) -> U128 {
+        self.get_account(account_id).staked_balance
+    }
+
+    /// Returns the total balance of the given account (including staked and unstaked balances).
+    pub fn get_account_total_balance(&self, account_id: AccountId) -> U128 {
+        let account = self.get_account(account_id);
+        (account.unstaked_balance.0 + account.staked_balance.0).into()
+    }
+
+    /// Returns `true` if the given account can withdraw tokens in the current epoch.
+    pub fn is_account_unstaked_balance_available(&self, account_id: AccountId) -> bool {
+        self.get_account(account_id).can_withdraw
+    }
+
+    /// Returns the total staking balance.
+    pub fn get_total_staked_balance(&self) -> U128 {
+        self.total_staked_near_amount.into()
+    }
+
+    /// Returns account ID of the staking pool owner.
+    pub fn get_owner_id(&self) -> AccountId {
+        self.owner_id.clone()
+    }
+
+    /// Returns the staking public key
+    pub fn get_staking_key(&self) -> PublicKey {
+        panic!("no need to specify public key for liquid staking pool");
+    }
+
+    /// Returns true if the staking is paused
+    pub fn is_staking_paused(&self) -> bool {
+        self.paused
+    }
+
+    /// Returns human readable representation of the account for the given account ID.
+    pub fn get_account(&self, account_id: AccountId) -> HumanReadableAccount {
+        let account = self.internal_get_account(&account_id);
+        HumanReadableAccount {
+            account_id: account_id.clone(),
+            unstaked_balance: account.unstaked.into(),
+            staked_balance: self
+                .staked_amount_from_num_shares_rounded_down(account.stake_shares)
+                .into(),
+            unstaked_available_epoch_height: account.unstaked_available_epoch_height,
+            can_withdraw: account.unstaked_available_epoch_height <= get_epoch_height(),
+            liquidity_pool_share: self.liquidity_pool.get_account_shares(&account_id).into(),
+            liquidity_pool_share_value: self.liquidity_pool.get_account_value(&account_id, &self.internal_get_context()).into(),
+            liquidity_pool_share_percentage: self.liquidity_pool.get_account_shares_percentage(&account_id),
+        }
+    }
+
+    /// Returns the number of accounts that have positive balance on this staking pool.
+    pub fn get_number_of_accounts(&self) -> u64 {
+        self.accounts.len()
+    }
+
+    /// Returns the list of accounts
+    pub fn get_accounts(&self, from_index: u64, limit: u64) -> Vec<HumanReadableAccount> {
+        let keys = self.accounts.keys_as_vector();
+
+        (from_index..std::cmp::min(from_index + limit, keys.len()))
+            .map(|index| self.get_account(keys.get(index).unwrap()))
+            .collect()
+    }
+
+
+    // --- Staking Farm view methods ---
 
     /// Return all authorized users.
     // pub fn get_authorized_users(&self) -> Vec<AccountId> {

6271b353ebddca553fc961cff4d619140a5731f8
dongcool <95206510+dongcool@users.noreply.github.com>
2022-03-13 16:03:06 +0800

    upgrade contract

--- contracts/linear/src/owner.rs
+++ contracts/linear/src/owner.rs
@@ -74,4 +74,51 @@ impl LiquidStakingContract {
         self.assert_owner();
         self.liquidity_pool.configure(config);
     }
+
+    // migration
+    #[init(ignore_state)]
+    #[private]
+    pub fn migrate() -> Self {
+        let contract: LiquidStakingContract = env::state_read().expect("ERR_NOT_INITIALIZED");
+        contract
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+mod upgrade {
+    use near_sys;
+    use near_sdk::Gas;
+
+    use super::*;
+
+    /// Gas for calling migration call.
+    pub const GAS_FOR_MIGRATE_CALL: Gas = Gas(5_000_000_000_000);
+
+    /// Self upgrade and call migrate, optimizes gas by not loading into memory the code.
+    /// Takes as input non serialized set of bytes of the code.
+    #[no_mangle]
+    pub extern "C" fn upgrade() {
+        env::setup_panic_hook();
+        let contract: LiquidStakingContract = env::state_read().expect("ERR_CONTRACT_IS_NOT_INITIALIZED");
+        contract.assert_owner();
+        let current_id = env::current_account_id().as_bytes().to_vec();
+        let method_name = "migrate".as_bytes().to_vec();
+        unsafe {
+            // Load input into register 0.
+            near_sys::input(0);
+            let promise_id =
+                near_sys::promise_batch_create(current_id.len() as _, current_id.as_ptr() as _);
+            near_sys::promise_batch_action_deploy_contract(promise_id, u64::MAX as _, 0);
+            let attached_gas = env::prepaid_gas() - env::used_gas() - GAS_FOR_MIGRATE_CALL;
+            near_sys::promise_batch_action_function_call(
+                promise_id,
+                method_name.len() as _,
+                method_name.as_ptr() as _,
+                0 as _,
+                0 as _,
+                0 as _,
+                attached_gas.0,
+            );
+        }
+    }
 }

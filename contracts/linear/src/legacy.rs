1af9628ebc252f916e1f9c53f888b8e082b6f71c
dongcool <95206510+dongcool@users.noreply.github.com>
2022-04-28 13:35:26 +0800

    pub;

--- contracts/linear/src/legacy.rs
+++ contracts/linear/src/legacy.rs
@@ -16,64 +16,64 @@ use near_sdk::{
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct ContractV1_1_0 {
     /// The account ID of the owner who's running the liquid staking contract.
-    owner_id: AccountId,
+    pub owner_id: AccountId,
     /// The accounts that are able to change key parameters and settings in the contract such as validator pool membership
-    managers: UnorderedSet<AccountId>,
+    pub managers: UnorderedSet<AccountId>,
     /// The account ID of the treasury that manages portion of the received fees and rewards.
-    treasury_id: AccountId,
+    pub treasury_id: AccountId,
     /// Total amount of LiNEAR that was minted (minus burned).
-    total_share_amount: ShareBalance,
+    pub total_share_amount: ShareBalance,
     /// Total amount of NEAR that was staked by users to this contract.         
     ///
     /// This is effectively 1) amount of NEAR that was deposited to this contract but hasn't yet been staked on any validators
     /// plus 2) amount of NEAR that has already been staked on validators.    
     /// Note that the amount of NEAR that is pending release or is already released by hasn't been withdrawn is not considered.
-    total_staked_near_amount: Balance,
+    pub total_staked_near_amount: Balance,
     /// Persistent map from an account ID to the corresponding account.
-    accounts: UnorderedMap<AccountId, Account>,
+    pub accounts: UnorderedMap<AccountId, Account>,
     /// Whether the staking is paused.
     /// When paused, the account unstakes everything (stakes 0) and doesn't restake.
     /// It doesn't affect the staking shares or reward distribution.
     /// Pausing is useful for node maintenance. Only the owner can pause and resume staking.
     /// The contract is not paused by default.
-    paused: bool,
+    pub paused: bool,
 
     /// The storage size in bytes for one account.
-    account_storage_usage: StorageUsage,
+    pub account_storage_usage: StorageUsage,
 
     /// Beneficiaries for staking rewards.
-    beneficiaries: UnorderedMap<AccountId, Fraction>,
+    pub beneficiaries: UnorderedMap<AccountId, u32>,
 
     /// The single-direction liquidity pool that enables instant unstake
-    liquidity_pool: LiquidityPool,
+    pub liquidity_pool: LiquidityPool,
 
     // --- Validator Pool ---
     /// The validator pool that manage the actions against validators
-    validator_pool: ValidatorPool,
+    pub validator_pool: ValidatorPool,
     /// Amount of NEAR that is requested to stake by all users during the last epoch
-    epoch_requested_stake_amount: Balance,
+    pub epoch_requested_stake_amount: Balance,
     /// Amount of NEAR that is requested to unstake by all users during the last epoch
-    epoch_requested_unstake_amount: Balance,
+    pub epoch_requested_unstake_amount: Balance,
 
     /// Amount of NEAR that needs to be settled by staking on validators
-    stake_amount_to_settle: Balance,
+    pub stake_amount_to_settle: Balance,
     /// Amount of NEAR that needs to be settled by unstaking from validators
-    unstake_amount_to_settle: Balance,
+    pub unstake_amount_to_settle: Balance,
     /// Last epoch height stake/unstake settlements were calculated
-    last_settlement_epoch: EpochHeight,
+    pub last_settlement_epoch: EpochHeight,
 
     // --- Staking Farm ---
     /// Farm tokens.
-    farms: Vector<Farm>,
+    pub farms: Vector<Farm>,
     /// Active farms: indicies into `farms`.
-    active_farms: Vec<u64>,
+    pub active_farms: Vec<u64>,
     /// Authorized users, allowed to add farms.
     /// This is done to prevent farm spam with random tokens.
     /// Should not be a large number.
     // authorized_users: UnorderedSet<AccountId>,
     /// Authorized tokens for farms.
     /// Required because any contract can call method with ft_transfer_call, so must verify that contract will accept it.
-    authorized_farm_tokens: UnorderedSet<AccountId>,
+    pub authorized_farm_tokens: UnorderedSet<AccountId>,
 }
 
 #[near_bindgen]

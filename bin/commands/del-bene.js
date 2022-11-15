fd436d7f8c9ff1f527cdc2e18f2f7b482f757764
dongcool <95206510+dongcool@users.noreply.github.com>
2022-11-15 14:56:19 +0800

    update script

--- bin/commands/del-bene.js
+++ bin/commands/del-bene.js
@@ -1,4 +1,4 @@
-const { init } = require("../near");
+const { init, funcCall } = require("../near");
 
 exports.command = 'del-bene <address>';
 exports.desc = 'Remove beneficiary';
@@ -20,23 +20,29 @@ exports.builder = yargs => {
       describe: 'beneficiary account ID'
     })
     .demandOption(['signer', 'account'])
+    .option('dao', {
+      describe: 'DAO address'
+    })
 }
 
 exports.handler = async function (argv) {
-  const { address, account } = argv;
+  const { address, dao, account } = argv;
   
   const near = await init(argv.network);
   const signer = await near.account(argv.signer);
 
   console.log(`removing beneficiary ${account}`);
 
-  await signer.functionCall({
-    contractId: address,
-    methodName: 'remove_beneficiary',
-    args: {
-      account_id: account,
+  await funcCall(
+    signer,
+    dao,
+    `Remove beneficiary ${account}`,
+    address,
+    'remove_beneficiary',
+    {
+      account_id: account
     }
-  });
+  )
 
   console.log('done');
 }

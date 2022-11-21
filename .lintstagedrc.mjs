dad581ee1244472bb799edf2738515d25ff06802
Linguists <95207870+linguists@users.noreply.github.com>
2022-11-21 21:45:20 +0800

    chore: update lint stage config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,11 +1,5 @@
 export default {
   "contracts/**/*.rs": [
     "rustfmt"
-  ],
-  "tests/**/*.ts?(x)": [
-    () => "tsc --project tsconfig.json --alwaysStrict",
-  ],
-  "tests/**/*.{js,jsx,ts,tsx}": [
-    "prettier --write"
-  ],
+  ]
 };

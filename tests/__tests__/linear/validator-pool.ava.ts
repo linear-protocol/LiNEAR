d7f807fb4b3ae4be5a9e713a9d2af77f340d1218
0xdefacto <107129842+0xdefacto@users.noreply.github.com>
2023-03-16 18:04:14 +0800

    total=10

--- tests/__tests__/linear/validator-pool.ava.ts
+++ tests/__tests__/linear/validator-pool.ava.ts
@@ -474,7 +474,7 @@ workspace.test('max update weights', async (test, context) => {
   let weights: number[] = [];
 
   let totalWeight = 0;
-  const total = 31;
+  const total = 10;
   for (let i = 0; i < total; i++) {
     totalWeight += i;
 

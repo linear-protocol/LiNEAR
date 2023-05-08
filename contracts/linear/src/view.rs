e77a5a5c4056245c3ab467a8bea58b77b1250185
0xdefacto <107129842+0xdefacto@users.noreply.github.com>
2023-05-08 14:40:58 +0000

    include epoch request amounts in summary

--- contracts/linear/src/view.rs
+++ contracts/linear/src/view.rs
@@ -44,6 +44,8 @@ pub struct Summary {
     pub stake_amount_to_settle: U128,
     /// Amount of NEAR that needs to be settled by unstaking from validators
     pub unstake_amount_to_settle: U128,
+    pub epoch_requested_stake_amount: U128,
+    pub epoch_requested_unstake_amount: U128,
     /// Total base stake amount of NEAR on validators
     pub validators_total_base_stake_amount: U128,
 }
@@ -106,6 +108,8 @@ impl LiquidStakingContract {
             farms: self.get_active_farms(),
             stake_amount_to_settle: self.stake_amount_to_settle.into(),
             unstake_amount_to_settle: self.unstake_amount_to_settle.into(),
+            epoch_requested_stake_amount: self.epoch_requested_stake_amount.into(),
+            epoch_requested_unstake_amount: self.epoch_requested_unstake_amount.into(),
             validators_total_base_stake_amount: self.validator_pool.total_base_stake_amount.into(),
         }
     }

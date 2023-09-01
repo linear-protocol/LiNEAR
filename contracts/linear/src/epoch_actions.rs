ecf08ed5627fe44c6b595d17559c5d9ba35648e8
0xdefacto <107129842+0xdefacto@users.noreply.github.com>
2023-09-01 19:03:26 +0800

    remove GAS_CB_VALIDATOR_RETURN_TRUE

--- contracts/linear/src/epoch_actions.rs
+++ contracts/linear/src/epoch_actions.rs
@@ -21,8 +21,7 @@ impl LiquidStakingContract {
             + GAS_EXT_DEPOSIT_AND_STAKE
             + GAS_CB_VALIDATOR_STAKED
             + GAS_SYNC_BALANCE
-            + GAS_CB_VALIDATOR_SYNC_BALANCE
-            + GAS_CB_VALIDATOR_RETURN_TRUE;
+            + GAS_CB_VALIDATOR_SYNC_BALANCE;
         require!(
             env::prepaid_gas() >= min_gas,
             format!("{}. require at least {:?}", ERR_NO_ENOUGH_GAS, min_gas)
@@ -75,10 +74,7 @@ impl LiquidStakingContract {
                 amount_to_stake.into(),
                 env::current_account_id(),
                 NO_DEPOSIT,
-                GAS_CB_VALIDATOR_STAKED
-                    + GAS_SYNC_BALANCE
-                    + GAS_CB_VALIDATOR_SYNC_BALANCE
-                    + GAS_CB_VALIDATOR_RETURN_TRUE,
+                GAS_CB_VALIDATOR_STAKED + GAS_SYNC_BALANCE + GAS_CB_VALIDATOR_SYNC_BALANCE,
             ))
             .into()
     }
@@ -90,8 +86,7 @@ impl LiquidStakingContract {
             + GAS_EXT_UNSTAKE
             + GAS_CB_VALIDATOR_UNSTAKED
             + GAS_SYNC_BALANCE
-            + GAS_CB_VALIDATOR_SYNC_BALANCE
-            + GAS_CB_VALIDATOR_RETURN_TRUE;
+            + GAS_CB_VALIDATOR_SYNC_BALANCE;
         require!(
             env::prepaid_gas() >= min_gas,
             format!("{}. require at least {:?}", ERR_NO_ENOUGH_GAS, min_gas)
@@ -138,10 +133,7 @@ impl LiquidStakingContract {
                 amount_to_unstake.into(),
                 env::current_account_id(),
                 NO_DEPOSIT,
-                GAS_CB_VALIDATOR_UNSTAKED
-                    + GAS_SYNC_BALANCE
-                    + GAS_CB_VALIDATOR_SYNC_BALANCE
-                    + GAS_CB_VALIDATOR_RETURN_TRUE,
+                GAS_CB_VALIDATOR_UNSTAKED + GAS_SYNC_BALANCE + GAS_CB_VALIDATOR_SYNC_BALANCE,
             ))
             .into()
     }

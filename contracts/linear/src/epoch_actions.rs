d1dcea1c8024235cce5ace3cb29169c243046b8d
Linguists <95207870+linguists@users.noreply.github.com>
2023-09-24 14:55:11 +0800

    feat: update minimum unstake amount

--- contracts/linear/src/epoch_actions.rs
+++ contracts/linear/src/epoch_actions.rs
@@ -7,7 +7,7 @@ use crate::types::*;
 use crate::utils::*;
 
 const MIN_AMOUNT_TO_PERFORM_STAKE: Balance = ONE_NEAR;
-const MIN_AMOUNT_TO_PERFORM_UNSTAKE: Balance = ONE_NEAR;
+const MIN_AMOUNT_TO_PERFORM_UNSTAKE: Balance = ONE_NEAR / 1000;
 const MAX_SYNC_BALANCE_DIFF: Balance = 100;
 const MANAGER_SYNC_BALANCE_DIFF_THRESHOLD: Balance = 1_000_000;
 

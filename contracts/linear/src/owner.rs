ee3088aa7abe62e571edabbab15c2774d1416b94
Linguists <95207870+linguists@users.noreply.github.com>
2024-05-07 14:16:31 +0800

    fix: hacken auditing finding F-2024-1975 (#197)

--- contracts/linear/src/owner.rs
+++ contracts/linear/src/owner.rs
@@ -1,3 +1,4 @@
+use crate::events::Event;
 use crate::*;
 use near_sdk::near_bindgen;
 
@@ -7,19 +8,36 @@ const MAX_BENEFICIARIES: u64 = 10;
 impl LiquidStakingContract {
     pub fn set_owner(&mut self, new_owner_id: AccountId) {
         self.assert_owner();
+        let old_owner_id = self.owner_id.clone();
         self.owner_id = new_owner_id;
+        Event::ChangeOwner {
+            old_owner_id: &old_owner_id,
+            new_owner_id: &self.owner_id,
+        }
+        .emit();
     }
 
     pub fn add_manager(&mut self, new_manager_id: AccountId) {
         self.assert_running();
         self.assert_owner();
         self.internal_add_manager(&new_manager_id);
+        Event::AddManager {
+            manager_id: &new_manager_id,
+        }
+        .emit();
     }
 
     pub fn remove_manager(&mut self, manager_id: AccountId) -> bool {
         self.assert_running();
         self.assert_owner();
-        self.internal_remove_manager(&manager_id)
+        let removed = self.internal_remove_manager(&manager_id);
+        if removed {
+            Event::RemoveManager {
+                manager_id: &manager_id,
+            }
+            .emit();
+        }
+        removed
     }
 
     pub fn set_beneficiary(&mut self, account_id: AccountId, bps: u32) {
@@ -43,26 +61,43 @@ impl LiquidStakingContract {
         );
 
         self.beneficiaries.insert(&account_id, &bps);
+        Event::SetBeneficiary {
+            account_id: &account_id,
+            bps: &bps,
+        }
+        .emit();
     }
 
     pub fn remove_beneficiary(&mut self, account_id: AccountId) {
         self.assert_running();
         self.assert_owner();
         self.beneficiaries.remove(&account_id);
+        Event::RemoveBeneficiary {
+            account_id: &account_id,
+        }
+        .emit();
     }
 
     /// Set account ID of the treasury
     pub fn set_treasury(&mut self, account_id: AccountId) {
         self.assert_running();
         self.assert_owner();
-        self.treasury_id = account_id;
+        self.treasury_id = account_id.clone();
+        Event::SetTreasury {
+            account_id: &account_id,
+        }
+        .emit();
     }
 
     /// Set whitelist account ID
     pub fn set_whitelist_contract_id(&mut self, account_id: AccountId) {
         self.assert_running();
         self.assert_owner();
-        self.whitelist_account_id = Some(account_id);
+        self.whitelist_account_id = Some(account_id.clone());
+        Event::SetWhitelist {
+            account_id: &account_id,
+        }
+        .emit();
     }
 
     // --- Pause ---
@@ -71,11 +106,13 @@ impl LiquidStakingContract {
         self.assert_owner();
         require!(!self.paused, ERR_ALREADY_PAUSED);
         self.paused = true;
+        Event::PauseContract {}.emit();
     }
 
     pub fn resume(&mut self) {
         self.assert_owner();
         require!(self.paused, ERR_NOT_PAUSED);
         self.paused = false;
+        Event::ResumeContract {}.emit();
     }
 }
